024822ec91639551e0b4d8c23a07484ff15085d6
Maria <mariatimina2005@gmail.com>
2025-10-15 00:06:22 +0300

    добавлена модель заметки

---
+++ backend/models/note.js
@@ -0,0 +1,9 @@
+const {DataTypes} = require('sequelize');
+const sequelize = require('../config/database');
+
+const Note = sequelize.define('Note', {
+    id: {type: DataTypes.INTEGER, primaryKey:true, autoIncrement: true},
+    title: {type: DataTypes.STRING, allowNull: true},
+    text: {type: DataTypes.STRING, allowNull: false},
+});
+module.exports = Note;
